29163a899d791a55014a30e4e781996c4e24e619
Rohith Vishwajith <rohithv@utexas.edu>
2024-02-02 00:29:31 -0600

    Update light.h

--- ray/src/scene/light.h
+++ ray/src/scene/light.h
@@ -11,90 +11,97 @@ using std::min;
 #include "scene.h"
 #include <FL/gl.h>
 
-class Light : public SceneElement {
+class Light : public SceneElement
+{
 public:
-  virtual glm::dvec3 shadowAttenuation(const ray &r,
-                                       const glm::dvec3 &pos) const = 0;
-  virtual double distanceAttenuation(const glm::dvec3 &P) const = 0;
-  virtual glm::dvec3 getColor() const = 0;
-  virtual glm::dvec3 getDirection(const glm::dvec3 &P) const = 0;
-  virtual glm::dvec3 getRelativeDirection(const glm::dvec3 &P) const = 0;
-  bool isPoint(){
-      return pointLight;
-  }
+	virtual glm::dvec3 shadowAttenuation(const ray &r,
+										 const glm::dvec3 &pos) const = 0;
+	virtual double distanceAttenuation(const glm::dvec3 &P) const = 0;
+	virtual glm::dvec3 getColor() const = 0;
+	virtual glm::dvec3 getDirection(const glm::dvec3 &P) const = 0;
+	virtual glm::dvec3 getRelativeDirection(const glm::dvec3 &P) const = 0;
+	bool isPoint()
+	{
+		return pointLight;
+	}
 
 protected:
-  Light(Scene *scene, const glm::dvec3 &col)
-      : SceneElement(scene), color(col) {}
+	Light(Scene *scene, const glm::dvec3 &col)
+		: SceneElement(scene), color(col) {}
 
-  glm::dvec3 color;
-  bool pointLight;
+	glm::dvec3 color;
+	bool pointLight;
 
 public:
-  virtual void glDrawLight([[maybe_unused]] GLenum lightID) const {}
-  virtual void glDrawLight() const {}
+	virtual void glDrawLight([[maybe_unused]] GLenum lightID) const {}
+	virtual void glDrawLight() const {}
 };
 
-class DirectionalLight : public Light {
+class DirectionalLight : public Light
+{
 public:
-  DirectionalLight(Scene *scene, const glm::dvec3 &orien,
-                   const glm::dvec3 &color)
-      : Light(scene, color), orientation(glm::normalize(orien)) {
-      pointLight = false;
-  }
-  virtual glm::dvec3 shadowAttenuation(const ray &r,
-                                       const glm::dvec3 &pos) const;
-  virtual double distanceAttenuation(const glm::dvec3 &P) const;
-  virtual glm::dvec3 getColor() const;
-  virtual glm::dvec3 getDirection(const glm::dvec3 &P) const;
-  virtual glm::dvec3 getRelativeDirection(const glm::dvec3 &P) const;
+	DirectionalLight(Scene *scene, const glm::dvec3 &orien,
+					 const glm::dvec3 &color)
+		: Light(scene, color), orientation(glm::normalize(orien))
+	{
+		pointLight = false;
+	}
+	virtual glm::dvec3 shadowAttenuation(const ray &r,
+										 const glm::dvec3 &pos) const;
+	virtual double distanceAttenuation(const glm::dvec3 &P) const;
+	virtual glm::dvec3 getColor() const;
+	virtual glm::dvec3 getDirection(const glm::dvec3 &P) const;
+	virtual glm::dvec3 getRelativeDirection(const glm::dvec3 &P) const;
 
 protected:
-  glm::dvec3 orientation;
+	glm::dvec3 orientation;
 
 public:
-  void glDrawLight(GLenum lightID) const;
-  void glDrawLight() const;
+	void glDrawLight(GLenum lightID) const;
+	void glDrawLight() const;
 };
 
-class PointLight : public Light {
+class PointLight : public Light
+{
 public:
-  PointLight(Scene *scene, const glm::dvec3 &pos, const glm::dvec3 &color,
-             float constantAttenuationTerm, float linearAttenuationTerm,
-             float quadraticAttenuationTerm)
-      : Light(scene, color), position(pos),
-        constantTerm(constantAttenuationTerm),
-        linearTerm(linearAttenuationTerm),
-        quadraticTerm(quadraticAttenuationTerm){
-      pointLight = true;
-  }
+	PointLight(Scene *scene, const glm::dvec3 &pos, const glm::dvec3 &color,
+			   float constantAttenuationTerm, float linearAttenuationTerm,
+			   float quadraticAttenuationTerm)
+		: Light(scene, color), position(pos),
+		  constantTerm(constantAttenuationTerm),
+		  linearTerm(linearAttenuationTerm),
+		  quadraticTerm(quadraticAttenuationTerm)
+	{
+		pointLight = true;
+	}
 
-  virtual glm::dvec3 shadowAttenuation(const ray &r,
-                                       const glm::dvec3 &pos) const;
-  virtual double distanceAttenuation(const glm::dvec3 &P) const;
-  virtual glm::dvec3 getColor() const;
-  virtual glm::dvec3 getDirection(const glm::dvec3 &P) const;
-  virtual glm::dvec3 getRelativeDirection(const glm::dvec3 &P) const;
+	virtual glm::dvec3 shadowAttenuation(const ray &r,
+										 const glm::dvec3 &pos) const;
+	virtual double distanceAttenuation(const glm::dvec3 &P) const;
+	virtual glm::dvec3 getColor() const;
+	virtual glm::dvec3 getDirection(const glm::dvec3 &P) const;
+	virtual glm::dvec3 getRelativeDirection(const glm::dvec3 &P) const;
 
-  void setAttenuationConstants(float a, float b, float c) {
-    constantTerm = a;
-    linearTerm = b;
-    quadraticTerm = c;
-  }
+	void setAttenuationConstants(float a, float b, float c)
+	{
+		constantTerm = a;
+		linearTerm = b;
+		quadraticTerm = c;
+	}
 
 protected:
-  glm::dvec3 position;
+	glm::dvec3 position;
 
-  // These three values are the a, b, and c in the distance attenuation function
-  // (from the slide labelled "Intensity drop-off with distance"):
-  //    f(d) = min( 1, 1/( a + b d + c d^2 ) )
-  float constantTerm;  // a
-  float linearTerm;    // b
-  float quadraticTerm; // c
+	// These three values are the a, b, and c in the distance attenuation function
+	// (from the slide labelled "Intensity drop-off with distance"):
+	//    f(d) = min( 1, 1/( a + b d + c d^2 ) )
+	float constantTerm;	 // a
+	float linearTerm;	 // b
+	float quadraticTerm; // c
 
 public:
-  void glDrawLight(GLenum lightID) const;
-  void glDrawLight() const;
+	void glDrawLight(GLenum lightID) const;
+	void glDrawLight() const;
 
 protected:
 };
