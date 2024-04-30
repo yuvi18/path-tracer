ea2b66ac452499947e14547a32e660734c0d366c
theriser101@gmail.com <theriser101@gmail.com>
2024-04-30 14:48:24 -0500

    Area light framework

--- ray/src/scene/light.cpp
+++ ray/src/scene/light.cpp
@@ -84,5 +84,45 @@ glm::dvec3 PointLight::shadowAttenuation(const ray &r,
     return light;
 }
 
+double RectangleAreaLight::distanceAttenuation(const glm::dvec3 &P) const {
+    double distance = glm::distance(position, P);
+    double denom = constantTerm + linearTerm * distance + quadraticTerm * glm::pow(distance, 2);
+    return glm::min(1.0, 1 / denom);
+}
+
+glm::dvec3 RectangleAreaLight::getColor() const { return color; }
+
+glm::dvec3 RectangleAreaLight::getDirection(const glm::dvec3 &P) const {
+    return glm::normalize(position - P);
+}
+
+glm::dvec3 RectangleAreaLight::getRelativeDirection(const glm::dvec3 &P) const {
+    return position - P;
+}
+
+glm::dvec3 RectangleAreaLight::shadowAttenuation(const ray &r,
+                                         const glm::dvec3 &p) const {
+    glm::dvec3 light = getColor();
+    double lightT = glm::sqrt(glm::dot(position - p, position - p));
+    isect point;
+    ray shadowRay = r;
+    scene->intersect(shadowRay, point);
+    while(point.getT() < lightT){
+        //We intersected a material before the light. Now we need to get the other side to find the distance.
+        glm::dvec3 entry = shadowRay.at(point);
+        shadowRay.setPosition(shadowRay.at(point.getT() + RAY_EPSILON));
+        scene->intersect(shadowRay, point);
+        glm::dvec3 exit = shadowRay.at(point);
+        double distance = glm::distance(entry, exit);
+        //Found distance, now do the transulcent light formula
+        light *= glm::pow(point.getMaterial().kt(point), glm::dvec3(distance));
+        //Great, now get the next material's intersection and continue.
+        shadowRay.setPosition(shadowRay.at(point.getT() + RAY_EPSILON));
+        scene->intersect(shadowRay, point);
+        lightT = glm::sqrt(glm::dot(position - shadowRay.getPosition(), position - shadowRay.getPosition()));
+    }
+    return light;
+}
+
 #define VERBOSE 0
 
--- ray/src/scene/light.h
+++ ray/src/scene/light.h
@@ -106,4 +106,49 @@ public:
 protected:
 };
 
+class RectangleAreaLight : public Light
+{
+public:
+    RectangleAreaLight(Scene *scene, const glm::dvec3 &pos, const glm::dvec3 &color,
+               float constantAttenuationTerm, float linearAttenuationTerm,
+               float quadraticAttenuationTerm)
+            : Light(scene, color), position(pos),
+              constantTerm(constantAttenuationTerm),
+              linearTerm(linearAttenuationTerm),
+              quadraticTerm(quadraticAttenuationTerm)
+    {
+    }
+
+    virtual glm::dvec3 shadowAttenuation(const ray &r,
+                                         const glm::dvec3 &pos) const;
+    virtual double distanceAttenuation(const glm::dvec3 &P) const;
+    virtual glm::dvec3 getColor() const;
+    virtual glm::dvec3 getDirection(const glm::dvec3 &P) const;
+    virtual glm::dvec3 getRelativeDirection(const glm::dvec3 &P) const;
+
+    void setAttenuationConstants(float a, float b, float c)
+    {
+        constantTerm = a;
+        linearTerm = b;
+        quadraticTerm = c;
+    }
+
+protected:
+    glm::dvec3 position;
+
+    // These three values are the a, b, and c in the distance attenuation function
+    // (from the slide labelled "Intensity drop-off with distance"):
+    //    f(d) = min( 1, 1/( a + b d + c d^2 ) )
+    float constantTerm;	 // a
+    float linearTerm;	 // b
+    float quadraticTerm; // c
+
+public:
+
+    //Debugging, don't know why it bugs when not having this
+//    void glDrawLight(GLenum lightID) const;
+//    void glDrawLight() const;
+
+};
+
 #endif // __LIGHT_H__
